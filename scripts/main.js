76a64946bfd082bcfa5cc025c0921ebc54cd4c27
Dana <dana.lachman@gmail.com>
2016-03-07 21:23:04 -0600

    paint calculator adventurer mode

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,14 @@ width = prompt("How wide is the room?")
 
 gallons = Math.ceil(length * width / 350)
 
+if(isNaN(length)) {
+	document.write("That's not a number, jerk. Stop trying to break my program.")
+} 
 
-document.write('<div class=\'write\'><p>You need to paint ' + length * width + ' square feet. You will need ' + gallons + ' gallon(s) of paint.</p></div')
+else if(isNaN(width)) {
+	document.write("That's not a number, jerk. Stop trying to break my program.")
+}
 
+else {
+	document.write('<div class=\'write\'><p>You need to paint ' + length * width + ' square feet. You will need ' + gallons + ' gallon(s) of paint.</p></div')
+}
